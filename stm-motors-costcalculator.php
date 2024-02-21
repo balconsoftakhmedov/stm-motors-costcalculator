cd0e1c313785d26d4984eae9f2fe9969d02648cf
Rusty <proga100@gmail.com>
2024-02-21 12:19:08 +0500

    ss

--- stm-motors-costcalculator.php
+++ stm-motors-costcalculator.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Plugin Name: STM Motors Custom Fleetdirect
+ * Plugin Name: STM Costcaluculator Custom
  * Plugin URI:  https://stylemixthemes.com/
- * Description: STM Motors Extends WordPress Plugin
- * Version:     3.0.0
- * Author:      StylemixThemes
+ * Description: STM WordPress Csutom Plugin
+ * Version:     1.0.0
+ * Author:      rusty
  * Author URI:  https://stylemixthemes.com/
  * License:     GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: stm_motors_custom
+ * Text Domain: stm-motors-costcalculator
  * Domain Path: /languages
  */
 
@@ -19,25 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 //error_reporting(E_ALL);
 //ini_set('display_errors', 1);
 
-define( 'MOTORS_ELEMENTOR_CUSTOMS_PATH', dirname( __FILE__ ) );
-define( 'MOTORS_ELEMENTOR_CUSTOMS_URL', plugins_url( '', __FILE__ ) );
+define( 'STM_ELEMENTOR_CUSTOMS_PATH', dirname( __FILE__ ) );
+define( 'STM_ELEMENTOR_CUSTOMS_URL', plugins_url( '', __FILE__ ) );
 include_once 'inc/loader.php';
-
-
-function register_custom_image_categories_widget() {
-    \Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Motors_E_W\Widgets\CustomImageCategories() );
-	\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new \Motors_E_W\Widgets\PassengerCustomImageCategories() );
-
-}
-
-function custom_image_categories_plugin_loaded() {
-	if ( class_exists( 'Motors_E_W\MotorsApp' ) ) {
-		require_once plugin_dir_path( __FILE__ ) . 'inc/Widgets/CustomImageCategories.php';
-		require_once plugin_dir_path( __FILE__ ) . 'inc/Widgets/PassengerCustomImageCategories.php';
-		add_action( 'elementor/widgets/widgets_registered', 'register_custom_image_categories_widget' );
-	}
-}
-
-add_action( 'init', 'custom_image_categories_plugin_loaded', 9999 );
-
 
