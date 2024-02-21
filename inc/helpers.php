53df166b3fdaddbf9c769da5914eac9f8b8775b3
Rusty <proga100@gmail.com>
2024-02-21 21:52:06 +0500

    dd

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,2 +1,13 @@
 <?php
 
+function stm_customs_enqueue_scripts() {
+	$v = time();
+	wp_enqueue_style( 'stm-style-customs', STM_ELEMENTOR_CUSTOMS_URL . '/assets/css/style.css', array(), time() );
+}
+
+function stm_customs_enqueue_on_plugins_loaded() {
+	add_action( 'wp_enqueue_scripts', 'stm_customs_enqueue_scripts', 999 );
+
+}
+
+add_action( 'init', 'stm_customs_enqueue_on_plugins_loaded', 9999 );
