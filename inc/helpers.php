e5576387e665cf1db099a59d5883bd8d3b16cc4e
Rusty <proga100@gmail.com>
2024-02-21 22:02:37 +0500

    wer

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use cBuilder\Helpers\CCBFieldsHelper;
+
 function stm_customs_enqueue_scripts() {
 	$v = time();
 	wp_enqueue_style( 'stm-style-customs', STM_ELEMENTOR_CUSTOMS_URL . '/assets/css/style.css', array(), time() );
@@ -7,7 +9,14 @@ function stm_customs_enqueue_scripts() {
 
 function stm_customs_enqueue_on_plugins_loaded() {
 	add_action( 'wp_enqueue_scripts', 'stm_customs_enqueue_scripts', 999 );
-
+	if ( ! is_admin() || ! empty( $_GET['page'] ) && 'cost_calculator_builder' === $_GET['action'] ) {  // phpcs:ignore WordPress.Security.NonceVerification
+			wp_enqueue_script( 'calc-builder-main-js', STM_ELEMENTOR_CUSTOMS_URL . '/assets/bundle.js', array( 'cbb-sticky-sidebar-js' ), time(), true );
+	}
 }
 
 add_action( 'init', 'stm_customs_enqueue_on_plugins_loaded', 9999 );
+
+
+if ( ! is_admin() || ! empty( $_GET['page'] ) && 'cost_calculator_builder' === $_GET['action'] ) {  // phpcs:ignore WordPress.Security.NonceVerification
+			wp_enqueue_script( 'calc-builder-main-js', STM_ELEMENTOR_CUSTOMS_URL . '/assets/js/bundle.js', array( 'cbb-sticky-sidebar-js' ), time(), true );
+}
